114bd6747cb8c70252274a63f2f732e67fc2461c
Trixi Turny <trixi.turny@gmail.com>
2016-09-29 13:43:01 +0100

    Start

--- app.js
+++ app.js
@@ -3,22 +3,60 @@ var fs = require('fs');
 var formidable = require("formidable");
 var express = require("express");
 var util = require("util");
+var request = require('request');
 var app = express();
+var EventEmitter = require("events").EventEmitter;
+var ee = new EventEmitter();
+var objectArray = {};
 
-var server = http.createServer(function(req, res) {
-   displayForm(res);
-});
+app.use(express.static('public'));
 
-function displayForm(res) {
-  fs.readFile('index.html', function(err, data) {
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
-      'Content-Length': data.length
-    });
-    res.write(data);
-    res.end();
-  });
-}
-server.listen(8080);
+var cheerio = require('cheerio');
+var $;
+
+app.get('/', function(request, response){
+
+  response.sendFile(__dirname+'/public/index.html');
+
+});app.listen(8080);
+
+
+// function displayForm(res) {
+//   fs.readFile('index.html', function(err, data) {
+//     res.writeHead(200, {
+//       'Content-Type': 'text/html',
+//       'Content-Length': data.length
+//     });
+
+//     html = data.toString();
+//     $ = cheerio.load(html);
+//     manipulate($);
+//     res.write(data);
+//     res.end();
+//   });
+// }
+
+// function manipulate($) {
+
+//   ee.on("submit", function(req, res) {
+//     res.preventDefault();
+//     console.log("event has occured");
+//     $('textarea').each(function(i, element) {
+//       var value = $(this).attr("placeholder");
+//       objectArray[i] = value;
+//       console.log(objectArray[i]);
+//     });
+//     $.html();
+//   });
+//   ee.emit("submit");
+// }
+
+// server.listen(8080);
 console.log("server listening on 8080");
 
+// function addElements(html) {
+//   $ = cheerio.load(html);
+//   var element = $("#trixi");
+//   console.log($(element).val());
+//   $.html()
+// }
