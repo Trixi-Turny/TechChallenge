9884119926defcabd8d588ca9d36ccd6cbaade46
Trixi Turny <beatrix.turny@cohaesus.co.uk>
2016-10-04 16:49:13 +0100

    Loading the First Story

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var partial = require('express-partial')
 var app = express();
 var request = require('request')
 var Node = require("tree-node")
-var index = 0;
 var stories = [];
 var centerStories = [];
 var storyBook = [];
 var storyStarter = "Once upon a time, there was a big bad wolf."
 var defaultCenter = storyStarter
+var title = "Trixi Turny - Tech Challenge"
 var storyLine0;
 var storyLine1;
 var storyLine2;
@@ -47,51 +47,47 @@ n4.data({
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
-      title: 'Trixi Turny - Tech Challenge',
+      title: title,
       centerStory: storyStarter
-        // storyLine: defaultContent
     });
   })
   /* POST */
 
-var helper = 0;
 router.post('/', function(req, res, next) {
-  for (var i = 0; i < Node.length; i++) {
+  // for (var i = 0; i < Node.length; i++) {
 
-    if (req.body.textarea0) {
-      n1.storyLine = req.body.textarea0
-      stories[0] = ({
-        "storyLine1": n1.storyLine
-      });;
-    } else if (req.body.textarea1) {
-      n2.storyLine = req.body.textarea1
-      stories[1] = ({
-        "storyLine2": n2.storyLine
-      });
+  if (req.body.textarea0) {
+    n1.storyLine = req.body.textarea0
+    stories[0] = ({
+      "storyLine1": n1.storyLine
+    });;
+  } else if (req.body.textarea1) {
+    n2.storyLine = req.body.textarea1
+    stories[1] = ({
+      "storyLine2": n2.storyLine
+    });
 
-    } else if (req.body.textarea2) {
-      n3.storyLine = req.body.textarea2
-      stories[2] = ({
-        "storyLine3": n3.storyLine
-      });
+  } else if (req.body.textarea2) {
+    n3.storyLine = req.body.textarea2
+    stories[2] = ({
+      "storyLine3": n3.storyLine
+    });
 
-    } else if (req.body.textarea3) {
-      n4.storyLine = req.body.textarea3
-      stories[3] = ({
-        "storyLine4": n4.storyLine
-      });
-    }
-    console.log(root.layer(n1.id));
-    res.render('index', {
-      storyLine0: n1.storyLine,
-      storyLine1: n2.storyLine,
-      storyLine2: n3.storyLine,
-      storyLine3: n4.storyLine,
-      centerStory: defaultCenter,
-      title: 'Trixi Turny - Tech Challenge',
-    })
+  } else if (req.body.textarea3) {
+    n4.storyLine = req.body.textarea3
+    stories[3] = ({
+      "storyLine4": n4.storyLine
+    });
   }
-  helper = helper + 1;
+
+  res.render('index', {
+    storyLine0: n1.storyLine,
+    storyLine1: n2.storyLine,
+    storyLine2: n3.storyLine,
+    storyLine3: n4.storyLine,
+    centerStory: defaultCenter,
+    title: title
+  })
 });
 
 
@@ -99,13 +95,12 @@ var storyCounter = 0;
 
 var counter = 0;
 centerStories.push(storyStarter);
-
+storyBook.push(stories)
 router.get('/:id', function(req, res, next) {
-  // storyBook[counter] = stories;
-  storyBook.push(stories)
+
   var index = req.params.id;
   if (index == 0) {
-    // root.up(n1.index);
+
     defaultCenter = n1.storyLine;
     centerStories.push(defaultCenter);
     n1.storyLine = "";
@@ -119,7 +114,7 @@ router.get('/:id', function(req, res, next) {
     n2.storyLine = "";
     n3.storyLine = "";
     n4.storyLine = "";
-    // root.up(n2.index);
+
   } else if (index == 2) {
     defaultCenter = n3.storyLine;
     centerStories.push(defaultCenter);
@@ -127,7 +122,7 @@ router.get('/:id', function(req, res, next) {
     n2.storyLine = "";
     n3.storyLine = "";
     n4.storyLine = "";
-    // root.up(n3.index)
+
   } else if (index == 3) {
     defaultCenter = n4.storyLine;
     centerStories.push(defaultCenter);
@@ -135,19 +130,15 @@ router.get('/:id', function(req, res, next) {
     n2.storyLine = "";
     n3.storyLine = "";
     n4.storyLine = "";
-    // root.up(n4.index)
-  } else if (index == "start") {
-    console.log(stories);
-    n1.storyLine = storyBook[storyCounter][storyCounter].storyLine1
-    n2.storyLine = storyBook[storyCounter][storyCounter + 1].storyLine2
-    n3.storyLine = storyBook[storyCounter][storyCounter + 2].storyLine3
-    n4.storyLine = storyBook[storyCounter][storyCounter + 3].storyLine4
-    defaultCenter = centerStories[storyCounter];
-    storyCounter = storyCounter + 1;
-  }
-  console.log(centerStories);
-  console.log(stories);
 
+  } else if (index == "start") {
+
+    n1.storyLine = "" + storyBook[storyCounter][storyCounter].storyLine1 // Undefined if empty
+    n2.storyLine = "" + storyBook[storyCounter][storyCounter + 1].storyLine2 // Undefined if empty
+    n3.storyLine = "" + storyBook[storyCounter][storyCounter + 2].storyLine3 // Undefined if empty
+    n4.storyLine = "" + storyBook[storyCounter][storyCounter + 3].storyLine4 // Undefined if empty
+    defaultCenter = centerStories[storyCounter];
+  }
 
   res.render('index', {
     storyLine0: n1.storyLine,
@@ -155,10 +146,8 @@ router.get('/:id', function(req, res, next) {
     storyLine2: n3.storyLine,
     storyLine3: n4.storyLine,
     centerStory: defaultCenter,
-    title: 'Trixi Turny - Tech Challenge'
+    title: title
   });
-
-  console.log(storyBook[storyCounter][storyCounter].storyLine1);
   counter = counter + 1;
 
 });
